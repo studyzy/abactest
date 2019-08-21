9a6fad449ee506665a9b05ab1d8276b1f54e7e62
Devin <studyzy@gmail.com>
2019-08-21 17:24:01 +0800

    Query rule and check permission

--- contract.go
+++ contract.go
@@ -7,39 +7,31 @@ import (
 	"github.com/casbin/casbin/model"
 )
 
-type Contract struct {
-	Creator  string
-	FuncName string
-	Address  string
-}
-func (c *Contract) Owner() string{
-	return c.Creator
-}
-func Init() string{
+func QueryPermissionRule(contractAddr string) string {
 	//GroupA能够访问ContractA的方法Fun1,Fun2
 	//GroupB能够访问ContractA的方法Fun3，Fun4
 	//ContractA创建者能够访问ContractA的所有方法
-	ruleA:= "m = (r.sub.Group==\"A\" && (r.obj.FuncName==\"Fun1\"|| r.obj.FuncName==\"Fun2\"))"
-	ruleA+="|| (r.sub.Group==\"B\" && r.obj.FuncName in (\"Fun3\",\"Fun4\"))"
-	ruleA+="|| r.sub.UserName==r.obj.Owner()"
+	ruleA := "m = (r.sub.Group==\"A\" && r.obj in ('Fun1','Fun2'))"
+	ruleA += "|| (r.sub.Group==\"B\" && r.obj in (\"Fun3\",\"Fun4\"))"
+	// ruleA+="|| r.sub.UserName==r.obj.Owner()"
 
 	return ruleA
 }
-func CheckContractPermission(u *User,contractAddr string,function  string) {
+func CheckContractPermission(u *User, contractAddr string, function string) {
 	m := model.Model{}
-	rule:=modelText + Init()
+	rule := modelText + QueryPermissionRule(contractAddr)
 	m.LoadModelFromText(rule)
 	e := casbin.NewEnforcer(m)
-	contract := &Contract{Creator: "User1", Address: contractAddr, FuncName: function}
+	// contract := &Contract{Creator: "User1", Address: contractAddr, FuncName: function}
 	//fmt.Println(rule)
-	flag := e.Enforce(u, contract)
+	flag := e.Enforce(u, function)
 	fmt.Println(contractAddr, u.UserName, function, flag)
 }
-func TestContractPermission(users []*User){
-	for _,u:=range users{
-		CheckContractPermission(u,"ContractA","Fun1")
-		CheckContractPermission(u,"ContractA","Fun2")
-		CheckContractPermission(u,"ContractA","Fun3")
-		CheckContractPermission(u,"ContractA","Fun4")
+func TestContractPermission(users []*User) {
+	for _, u := range users {
+		CheckContractPermission(u, "ContractA", "Fun1")
+		CheckContractPermission(u, "ContractA", "Fun2")
+		CheckContractPermission(u, "ContractA", "Fun3")
+		CheckContractPermission(u, "ContractA", "Fun4")
 	}
 }
--- token.go
+++ token.go
@@ -11,16 +11,15 @@ type Token struct{
 	PermissionRule string
 }
 func TestTokenPermission(users []*User){
-	tokenA:=&Token{TokenName:"TokenA",
-		PermissionRule:"m = r.sub.Group == \"A\""}
+	tokenA:=&Token{TokenName:"TokenA",}
 	tokenB:=&Token{TokenName:"TokenB",
-		PermissionRule:"m = r.sub.Group == \"B\""}
+	}
 	tokenC:=&Token{TokenName:"TokenC",
-		PermissionRule:"m = r.sub.Credit >= 60"}
+	}
 	tokens:=[]*Token{tokenA,tokenB,tokenC}
 	for _,token:=range tokens{
 		m := model.Model{}
-		m.LoadModelFromText(modelText+token.PermissionRule)
+		m.LoadModelFromText(modelText+QueryTokenPermissionRule(token.TokenName))
 		e := casbin.NewEnforcer(m)
 		for _, u := range users {
 			//强制决定一个“subject”是否可以通过操作“action”访问一个“object”，输入参数通常是:(sub, obj, act)。
@@ -32,4 +31,16 @@ func TestTokenPermission(users []*User){
 			}
 		}
 	}
+}
+func QueryTokenPermissionRule(token string) string{
+	switch 
+	 {
+		case token=="TokenA":
+			return "m = r.sub.Group == \"A\""
+		case token =="TokenB":
+			return "m = r.sub.Group == \"B\""
+		case token== "TokenC":
+			return "m = r.sub.Credit >= 60"
+	}
+	return ""
 }
